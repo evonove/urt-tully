0bb10bbc864b0face5d7408797e1fa4387aab440
Luca Treppaoli <lucatreppaoli@gmail.com>
2016-08-17 11:08:50 +0200

    third commit

--- django-tully/tully/wsgi.py
+++ django-tully/tully/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for unipgExample project.
+WSGI config for tully project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unipgExample.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tully.settings")
 
 application = get_wsgi_application()
