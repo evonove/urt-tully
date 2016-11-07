4c58bb636bf8ecae1e39821de7203fe3ffd9cf0c
Luca Treppaoli <lucatreppaoli@gmail.com>
2016-11-07 11:30:49 +0100

    changed models.py file

--- django-tully/tully/settings.py
+++ django-tully/tully/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os, dj_database_url
+import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,5 +141,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE':10,
+    'PAGE_SIZE': 10,
 }
